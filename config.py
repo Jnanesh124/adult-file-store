3c51044ed058a5a715fe33044827820fc42ceabc
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-12-29 14:31:26 +0600

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002108419450"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<blockquote>Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.</blockquote>")
+START_MSG = os.environ.get("START_MESSAGE", "<blockquote>I can store private files in Specified Channel and other users can access it from special link.\n\n{uptime}</blockquote>")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "6643562770 6643562770 6643562770").split()):
