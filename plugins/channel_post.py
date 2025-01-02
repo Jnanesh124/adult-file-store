74a1586e300ca5bef406d4db22510c2ce2e95a1b
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2025-01-02 23:18:51 +0600

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -5,15 +5,15 @@ from pyrogram.errors import FloodWait
 from moviepy.editor import VideoFileClip
 from PIL import Image
 import os
-import io
 
 from bot import Bot
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
+@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start', 'users', 'broadcast', 'batch', 'genlink', 'stats']))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Please Wait...!", quote=True)
+
     try:
         # Handle video or media files
         if message.video:
@@ -26,31 +26,47 @@ async def channel_post(client: Client, message: Message):
             image = Image.fromarray(frame)
             image.save(thumbnail_path)  # Save as a thumbnail image
 
-            # Send the thumbnail image
-            await message.reply_photo(photo=thumbnail_path, caption="Here is your video thumbnail!")
+            # Send the video to the channel
+            post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
 
-            # Remove the downloaded video and thumbnail after use
+            # Remove the downloaded video after use
             os.remove(video_path)
-            os.remove(thumbnail_path)
         else:
             # If no video, just proceed with the usual link generation
             post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
+            thumbnail_path = None
     except FloodWait as e:
         await asyncio.sleep(e.value)
         post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
     except Exception as e:
         print(e)
-        await reply_text.edit_text("Something went Wrong..!")
+        await reply_text.edit_text("Something went wrong while processing!")
         return
 
+    # Generate the stored link
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]]
+    )
 
-    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview=True)
+    # Send both the thumbnail and the link
+    if thumbnail_path:
+        await message.reply_photo(
+            photo=thumbnail_path,
+            caption=f"<b>Here is your link</b>\n\n{link}",
+            reply_markup=reply_markup
+        )
+        os.remove(thumbnail_path)  # Clean up the thumbnail
+    else:
+        await reply_text.edit(
+            f"<b>Here is your link</b>\n\n{link}",
+            reply_markup=reply_markup,
+            disable_web_page_preview=True
+        )
 
     if not DISABLE_CHANNEL_BUTTON:
         try:
@@ -61,18 +77,21 @@ async def channel_post(client: Client, message: Message):
         except Exception:
             pass
 
+
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
 async def new_post(client: Client, message: Message):
     if DISABLE_CHANNEL_BUTTON:
         return
 
-    # Generate the link as before
+    # Generate the stored link
     converted_id = message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]]
+    )
 
     try:
         await message.edit_reply_markup(reply_markup)
