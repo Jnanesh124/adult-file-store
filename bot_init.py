c193491955ad6bc71f99c2a1d156cafafecea371
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2025-01-03 16:20:42 +0600

    Create bot_init.py

---
+++ bot_init.py
@@ -0,0 +1,8 @@
+from pyrogram import Client
+
+Bot = Client(
+    "bot",
+    api_id=25166107,  # Replace with your API ID
+    api_hash="8a386f1bde254b67f917a9b2b436d0d6",  # Replace with your API hash
+    bot_token="6570037316:AAEU9Z4wvk5F6AU8EFeAXMf-gL_njebFMKg"  # Replace with your bot token
+)
