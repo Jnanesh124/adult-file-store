45e9ab8512b5c1f4c3873c84ab916066f4d2be42
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2025-01-04 19:21:03 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,3 @@
-# https://www.youtube.com/channel/UC7tAa4hho37iNv731_6RIOg
 import asyncio
 import base64
 import logging
@@ -21,32 +20,25 @@ from config import (
     CUSTOM_CAPTION,
     IS_VERIFY,
     VERIFY_EXPIRE,
-    SHORTLINK_API,
-    SHORTLINK_URL,
     DISABLE_CHANNEL_BUTTON,
     PROTECT_CONTENT,
     TUT_VID,
     OWNER_ID,
 )
-from helper_func import subscribed, encode, decode, get_messages, get_shortlink, get_verify_status, update_verify_status, get_exp_time
+from helper_func import subscribed, encode, decode, get_messages, get_verify_status, update_verify_status, get_exp_time
 from database.database import add_user, del_user, full_userbase, present_user
-from shortzy import Shortzy
 
 # Logging configuration
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
     owner_id = ADMINS  # Fetch the owner's ID from config
 
-    # Check if the user is the owner
     if id == owner_id:
-        # Owner-specific actions
         await message.reply("You are the owner! Additional actions can be added here.")
-
     else:
         if not await present_user(id):
             try:
@@ -61,11 +53,9 @@ async def start_command(client: Client, message: Message):
         if "verify_" in message.text:
             _, token = message.text.split("_", 1)
             if verify_status['verify_token'] != token:
-                return await message.reply("Your token is invalid or Expired. Try again by clicking /start")
+                return await message.reply("Your token is invalid or expired. Try again by clicking /start")
             await update_verify_status(id, is_verified=True, verified_time=time.time())
-            if verify_status["link"] == "":
-                reply_markup = None
-            await message.reply(f"Your token successfully verified and valid for: 24 Hour", reply_markup=reply_markup, protect_content=False, quote=True)
+            await message.reply("Your token is successfully verified and valid for 24 hours.", quote=True)
 
         elif len(message.text) > 7 and verify_status['is_verified']:
             try:
@@ -80,47 +70,32 @@ async def start_command(client: Client, message: Message):
                     end = int(int(argument[2]) / abs(client.db_channel.id))
                 except:
                     return
-                if start <= end:
-                    ids = range(start, end + 1)
-                else:
-                    ids = []
-                    i = start
-                    while True:
-                        ids.append(i)
-                        i -= 1
-                        if i < end:
-                            break
+                ids = range(start, end + 1) if start <= end else reversed(range(end, start + 1))
             elif len(argument) == 2:
                 try:
                     ids = [int(int(argument[1]) / abs(client.db_channel.id))]
                 except:
                     return
+
             temp_msg = await message.reply("Sending, please wait...")
             try:
                 messages = await get_messages(client, ids)
             except:
-                await message.reply_text("Something went wrong..!")
+                await message.reply_text("Something went wrong!")
                 return
             await temp_msg.delete()
-            
-            snt_msgs = []
-            
+
             for msg in messages:
-                if bool(CUSTOM_CAPTION) & bool(msg.document):
-                    caption = CUSTOM_CAPTION.format(
+                caption = (
+                    CUSTOM_CAPTION.format(
                         previouscaption="" if not msg.caption else msg.caption.html,
                         filename=msg.document.file_name
                     )
-                else:
-                    caption = "" if not msg.caption else msg.caption.html
-
-                if DISABLE_CHANNEL_BUTTON:
-                    reply_markup = msg.reply_markup
-                else:
-                    reply_markup = None
-
+                    if bool(CUSTOM_CAPTION) and bool(msg.document) else msg.caption.html
+                )
+                reply_markup = None if DISABLE_CHANNEL_BUTTON else msg.reply_markup
                 try:
-                    snt_msg = await msg.copy(
+                    await msg.copy(
                         chat_id=message.from_user.id,
                         caption=caption,
                         parse_mode=ParseMode.HTML,
@@ -128,27 +103,17 @@ async def start_command(client: Client, message: Message):
                         protect_content=PROTECT_CONTENT
                     )
                     await asyncio.sleep(0.5)
-                    snt_msgs.append(snt_msg)
                 except FloodWait as e:
                     await asyncio.sleep(e.x)
-                    snt_msg = await msg.copy(
+                    await msg.copy(
                         chat_id=message.from_user.id,
                         caption=caption,
                         parse_mode=ParseMode.HTML,
                         reply_markup=reply_markup,
                         protect_content=PROTECT_CONTENT
                     )
-                    snt_msgs.append(snt_msg)
                 except:
                     pass
-
-            # Follow-up message after sending the files
-            follow_up_msg = await client.send_message(
-                chat_id=message.from_user.id,
-                text="No link ❌ no ads ❌ Direct video ✅\n⛔️ kannada\n⛔️ nudi\n⛔️ oyo\n⛔️ Hidden cam\n⛔️ Rp\n⛔️ Brazzer \n⛔️ only fan\n⛔️ Celibrate\n⛔️ animal s€x\n⛔️ girls Lesbian \n⛔️ Desi\n⛔️ She Male\n⛔️ Savitha bhabhi \n\n👆🏻Abow all direct video 👆🏻\n\nJust :- 250₹ ( one time payment Lifetime free)\n\nTo Buy msg :- @msgmetobuy\n\n  ✅ All Are Video Uploaded In Different channel forwarding allows ✅"
-            )
-            await asyncio.sleep(500)  # Adjust the delay as needed
-            await follow_up_msg.delete()
 
         elif verify_status['is_verified']:
             reply_markup = InlineKeyboardMarkup(
@@ -166,72 +131,25 @@ async def start_command(client: Client, message: Message):
                 disable_web_page_preview=True,
                 quote=True
             )
-
         else:
-            verify_status = await get_verify_status(id)
             if IS_VERIFY and not verify_status['is_verified']:
-                short_url = f"adrinolinks.in"
-                # TUT_VID = f"https://t.me/How_to_open_link_rockersbot"
                 token = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
                 await update_verify_status(id, verify_token=token, link="")
-                link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API,f'https://telegram.dog/{client.username}?start=verify_{token}')
+                direct_link = f'https://telegram.dog/{client.username}?start=verify_{token}'
                 btn = [
-                    [InlineKeyboardButton("Verify", url=link)],
+                    [InlineKeyboardButton("Verify", url=direct_link)],
                     [InlineKeyboardButton('How To Verify', url=TUT_VID)]
                 ]
-                await message.reply(f"U need to verify 1 time\n\n<blockquote>after 24HOUR FREE DIRECT FILE</blockquote>", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
-
-
-        
-#=====================================================================================##
-
-WAIT_MSG = """"<b>Processing ...</b>"""
-
-REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
-
-#=====================================================================================##
-
-    
-    
-@Bot.on_message(filters.command('start') & filters.private)
-async def not_joined(client: Client, message: Message):
-    buttons = [
-        [
-            InlineKeyboardButton("join channel", url=f'https://t.me/+ksnFVzMfL4cyY2I1'),
-        ],[
-            InlineKeyboardButton(
-                "Join Channel",
-                url = client.invitelink),
-            InlineKeyboardButton("join channel", url=f'https://t.me/+MKBzevB1VYc2MGE1'),
-    ]]
-    try:
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text = 'Try Again',
-                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+                await message.reply(
+                    "You need to verify once to access files. After verification, you'll have 24 hours of free direct file access!",
+                    reply_markup=InlineKeyboardMarkup(btn),
+                    protect_content=False,
+                    quote=True
                 )
-            ]
-        )
-    except IndexError:
-        pass
-
-    await message.reply(
-        text = FORCE_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-        reply_markup = InlineKeyboardMarkup(buttons),
-        quote = True,
-        disable_web_page_preview = True
-    )
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
-    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
+    msg = await client.send_message(chat_id=message.chat.id, text="Processing...")
     users = await full_userbase()
     await msg.edit(f"{len(users)} users are using this bot")
 
@@ -240,13 +158,9 @@ async def send_text(client: Bot, message: Message):
     if message.reply_to_message:
         query = await full_userbase()
         broadcast_msg = message.reply_to_message
-        total = 0
-        successful = 0
-        blocked = 0
-        deleted = 0
-        unsuccessful = 0
+        total, successful, blocked, deleted, unsuccessful = 0, 0, 0, 0, 0
         
-        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
+        pls_wait = await message.reply("<i>Broadcasting Message... This will take some time.</i>")
         for chat_id in query:
             try:
                 await broadcast_msg.copy(chat_id)
@@ -265,18 +179,15 @@ async def send_text(client: Bot, message: Message):
                 unsuccessful += 1
                 pass
             total += 1
-        
-        status = f"""<b><u>Broadcast Completed</u>
 
-Total Users: <code>{total}</code>
-Successful: <code>{successful}</code>
-Blocked Users: <code>{blocked}</code>
-Deleted Accounts: <code>{deleted}</code>
-Unsuccessful: <code>{unsuccessful}</code></b>"""
-        
-        return await pls_wait.edit(status)
-
+        status = f"""<b><u>Broadcast Completed</u></b>\n
+Total Users: <code>{total}</code>\n
+Successful: <code>{successful}</code>\n
+Blocked Users: <code>{blocked}</code>\n
+Deleted Accounts: <code>{deleted}</code>\n
+Unsuccessful: <code>{unsuccessful}</code>"""
+        await pls_wait.edit(status)
     else:
-        msg = await message.reply(REPLY_ERROR)
+        msg = await message.reply("<code>Use this command as a reply to a message.</code>")
         await asyncio.sleep(8)
         await msg.delete()
