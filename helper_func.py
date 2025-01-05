580bd7d34f8b75e1bb0f2bb73c472d3293f8d34a
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2025-01-05 17:15:03 +0600

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -46,8 +46,11 @@ async def encode(string):
 async def decode(base64_string):
     base64_string = base64_string.strip("=")  # links generated before this commit will be having = sign, hence striping them to handle padding errors.
     base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
-    string_bytes = base64.urlsafe_b64decode(base64_bytes) 
-    string = string_bytes.decode("utf-8")  # Use UTF-8 instead of ASCII
+    try:
+        string_bytes = base64.urlsafe_b64decode(base64_bytes)
+        string = string_bytes.decode("utf-8")  # Use UTF-8 instead of ASCII
+    except UnicodeDecodeError:
+        string = string_bytes.decode("utf-8", errors="ignore")  # Handle decoding errors
     return string
 
 async def get_messages(client, message_ids):
