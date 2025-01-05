2d0f4c0a8f66e79f8d8f1263a2306e176be40194
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2025-01-05 16:58:05 +0600

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -34,7 +34,7 @@ async def batch(client: Client, message: Message):
 
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
-    link = f"https://jn2flix.blogspot.com/2025/01/file.html?JN2FLIX={base64_string}"
+    link = f"https://t.me/Adult_Video_Storej2_Bot?JN2FLIX={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await second_message.reply_text(f"<strong>480P 720P 720PHEVC 1080P 📂\n\n{link}\n\n▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️\nJoin Backup channel @JN2FLIX\n▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️</strong>", quote=True, reply_markup=reply_markup)
 
@@ -54,6 +54,6 @@ async def link_generator(client: Client, message: Message):
             continue
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
-    link = f"https://jn2flix.blogspot.com/2025/01/file.html?JN2FLIX={base64_string}"
+    link = f"https://t.me/Adult_Video_Storej2_Bot?JN2FLIX={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await channel_message.reply_text(f"<strong>480P 720P 720PHEVC 1080P 📂\n\n{link}\n\n▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️\nJoin Backup channel @JN2FLIX\n▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️▪️</strong>", quote=True, reply_markup=reply_markup)
