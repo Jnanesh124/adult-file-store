88f0a3b6afc9e8f0cfed8c485b43f102dda40f2f
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-01-28 14:58:15 +0600

    Update config.py

--- config.py
+++ config.py
@@ -21,6 +21,9 @@ BACKUP_BOTS = [
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002205211966"))
 
+ Define the chunk size for file uploads/downloads
+CHUNK_SIZE_MB = 500  # Example: 500MB
+
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "662770"))
 
