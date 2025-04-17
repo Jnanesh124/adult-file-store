26b5e99f0394fc4500b751125f2d0c253069597a
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-04-17 14:26:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ START_PIC = os.environ.get("START_PIC","https://envs.sh/Ype.jpg")
 #Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8011382U515mKPJwdo64")
 
-LINK = os.environ.get("LINK","youtube.com/@phdlust")
-CHAT = os.environ.get("CHAT","uofficial")
-CHANNEL = os.environ.get("CHANNEL","ultroidxteam")
+LINK = os.environ.get("LINK","https://t.me/ROCKERSBACKUP")
+CHAT = os.environ.get("CHAT","ROCKERSBACKUP")
+CHANNEL = os.environ.get("CHANNEL","ROCKERSBACKUP")
 
 # Auto delete time in seconds.
-AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "0"))
-AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "This file will be automatically deleted in {time} seconds. Please ensure you have saved any necessary content before this time.")
-AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "Your file has been successfully deleted. Thank you for using our service. ✅")
+AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "100"))
+AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "<strong>This file will be automatically deleted in {time} seconds. Please ensure you have saved any necessary content before this time.</strong>")
+AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "Your file has been successfully deleted. Thank you for using our service. ✅\n\n Join Free Loots channel https://t.me/JNKFREELOOTS ⚪")
 
 #Your API ID & API HASH from my.telegram.org [https://youtu.be/gZQJ-yTMkEo?si=H4NlUUgjsIc5btzH]
 #Your API ID from my.telegram.org
@@ -42,10 +42,10 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "inshorturl.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "9f943360c339cec4fed66d9d5cbaa0c2b3d41f81")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "inshort")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "9f943360ced66d9d5cbaa0c2b3d41f81")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/+PZfHvzjSiZc4OGE1") # shareus ka tut_vid he 
 
 #force sub channel id, if you want enable force sub
@@ -63,10 +63,10 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join all Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "This video/Photo/anything is available on the internet. We LeakHubd or its subsidiary channel doesn't produce any of them.")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
@@ -75,7 +75,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot! Join @ROCKERSBACKUP @JNKFREELOOTS"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6695586027)
