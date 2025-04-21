6fc93e6035a21503cc758cace1e7d6e6000df961
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-04-21 20:04:39 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -176,7 +176,7 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(
                 "Join Channel",
-                url = f"https://t.me/+gzwfJPJ-_uUzNjBl"),
+                url = f"https://t.me/+JL-Z9gmRBxY3ZDI9"),
             InlineKeyboardButton(
                 "Join Channel",
                 url = f"https://t.me/+KY2CkUJmVIE2N2Rl")
